db680bd86f551c8bafff5b9477a1ee59c0c9037d
lrr03 <luca.ratan03@yahoo.com>
2024-05-29 01:20:30 +0300

    Already finished with Spartacus MVC

--- UBB-SE-2024-Evil/wwwroot/js/Spartacus/GamePage.js
+++ UBB-SE-2024-Evil/wwwroot/js/Spartacus/GamePage.js
@@ -1,16 +1,20 @@
 ﻿window.onload = () => {
-    $('#submitButton').click(() => {
+    //console.log("Hello from GamePage.js");
+    // $('#submitButton').click(())
+    $('#moveForm').submit((e) => {
+        e.preventDefault();
         // Get the values from the input fields
         var inputDamage = parseInt($('#inputDamage').val());
         var inputBlock = parseInt($('#inputBlock').val());
+        //console.log("Hello");
 
-
-        // Send a PATCH request
+         //Send a PATCH request
         $.ajax({
             url: '/Game/DoMove?damage=' + inputDamage + '&block=' + inputBlock,
             type: 'PATCH',
             success: function (response) {
-                // Handle success response
+                //$('#inputDamage').text = "";
+                //$('#inputBlock').text = "";
             },
             error: function (xhr, status, error) {
                 // Handle error
