4a3f94590f426f092ca05f8023f178a8f27f37ba
lrr03 <luca.ratan03@yahoo.com>
2024-05-28 23:59:06 +0300

    Started the Spartacus MVC

--- UBB-SE-2024-Evil/Scripts/Spartacus/GamePage.js
+++ UBB-SE-2024-Evil/Scripts/Spartacus/GamePage.js
@@ -1,3 +1,33 @@
-﻿window.onload = () => {
+﻿//window.onload = () => {
 
-}
+//}
+//$(document).ready(function () {
+//    console.log("ENTERED JS");
+//    $('#gameForm').submit(function (e) {
+//        e.preventDefault(); // Prevent the default form submission
+
+//        // Get the values from the input fields
+//        //var input1Val = parseInt($('#input1').val());
+//        //var input2Val = oarseInt($('#input2').val());
+
+//        //// Create the Move object
+//        //var moveObject = {
+//        //    input1: input1Val,
+//        //    input2: input2Val
+//        //};
+
+//        //// Send a PATCH request
+//        //$.ajax({
+//        //    url: '/GameController/DoMove',
+//        //    type: 'PATCH',
+//        //    contentType: 'application/json',
+//        //    data: JSON.stringify(moveObject),
+//        //    success: function (response) {
+//        //        // Handle success response
+//        //    },
+//        //    error: function (xhr, status, error) {
+//        //        // Handle error
+//        //    }
+//        //});
+//    });
+//});
