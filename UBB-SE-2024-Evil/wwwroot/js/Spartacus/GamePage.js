a10a116b66d6d2e5c4ec253594288c63dea27a4f
Denis <93794328+denis-vp@users.noreply.github.com>
2024-05-29 00:22:08 +0300

    fix request

---
+++ UBB-SE-2024-Evil/wwwroot/js/Spartacus/GamePage.js
@@ -0,0 +1,28 @@
+﻿window.onload = () => {
+    var maxHealth = 200;
+
+    //$('#player-health-bar').css('width', (@Model.Player.Health / maxHealth * 100) + '%');
+    //$('#enemy-health-bar').css('width', (@Model.Enemy.Health / maxHealth * 100) + '%');
+
+    console.log("ENTERED JS");
+
+    $('#submitButton').click(() => {
+        // Get the values from the input fields
+        var inputDamage = parseInt($('#inputDamage').val());
+        var inputBlock = parseInt($('#inputBlock').val());
+
+
+        // // Send a PATCH request
+        // '@Url.Action("DoMove", "Game")'
+        $.ajax({
+            url: '/Game/DoMove?damage=' + inputDamage + '&block=' + inputBlock,
+            type: 'PATCH',
+            success: function (response) {
+                // Handle success response
+            },
+            error: function (xhr, status, error) {
+                // Handle error
+            }
+        });
+    });
+}
