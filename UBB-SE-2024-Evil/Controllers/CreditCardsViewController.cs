65e430f7229c1bfd687b3e0ae503fd219dd46374
Denis <93794328+denis-vp@users.noreply.github.com>
2024-05-28 20:30:46 +0300

    create files

---
+++ UBB-SE-2024-Evil/Controllers/CreditCardsViewController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UBB_SE_2024_Evil.Controllers
+{
+    public class CreditCardsViewController : Controller
+    {
+        public IActionResult X()
+        {
+            return View();
+        }
+
+        public IActionResult Subscription()
+        {
+            return View();
+        }
+    }
+}
