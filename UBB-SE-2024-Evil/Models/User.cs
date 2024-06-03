fcd69b9758bf52e3864e0577446c746e971039c3
tripondavid <davidtripon3@gmail.com>
2024-06-03 22:06:39 +0300

    Migrated backend logic from g927.

---
+++ UBB-SE-2024-Evil/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UBB_SE_2024_Evil.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        List<Bid> Bids { get; set; }
+        List<Auction> Auctions { get; set; }
+    }
+}
